536166399230ba793e26a101c03ff0c565272b45
Curtis Morte <curtismorte@gmail.com>
2019-04-01 12:20:44 -0700

    Setup / Save with Current Annotation Reason

--- src/components/AnnotationReasonPopup/AnnotationReasonPopup.js
+++ src/components/AnnotationReasonPopup/AnnotationReasonPopup.js
@@ -9,7 +9,12 @@ import selectors from 'selectors';
 
 import './AnnotationReasonPopup.scss';
 
-
+/**
+ * TODO:
+ *  - Fire some type of event letting the annotation manager know that the annotation was changed?
+ *  - Create a sub-component to pass handler and use props as the arguments for input
+ *    @see https://stackoverflow.com/questions/29810914/react-js-onclick-cant-pass-value-to-method
+ */
 class AnnotationReasonPopup extends React.Component {
   static propTypes = {
     isDisabled: PropTypes.bool,
@@ -17,57 +22,82 @@ class AnnotationReasonPopup extends React.Component {
     closeElement: PropTypes.func.isRequired
   };
 
+  constructor(props) {
+    super(props);
+
+    // Get the current value for a custom reason
+    let currentValue = '';
+    if(props.annotation.Reason && core.getCurrentReasons().indexOf(props.annotation.Reason) === -1){
+      currentValue = props.annotation.Reason;
+    }
+
+    this.state = {
+      inputValue: currentValue
+    };
+
+  }
+
   applyReason = reason => {
     // Close the annotation
     const {annotation, closeElement} = this.props;
     annotation.Reason = reason;
     closeElement('annotationPopup');
-    // TODO - Fire some type of event letting the annotation manager know that the annotation was changed?
+    console.log(annotation);
   };
 
   render() {
-    const {isDisabled} = this.props;
+    const {isDisabled, annotation} = this.props;
     const className = getClassName('Popup AnnotationReasonPopup', this.props);
-    const definedReasons = core.getReasons();
+    const currentReason = annotation.Reason;
+    const definedReasons = core.getCurrentReasons();
 
     if (isDisabled) {
       return null;
     }
 
-    if (definedReasons.constructor !== Array){
-      console.warn('Reasons expects an array, you passed', typeof(definedReasons), definedReasons);
+    if (definedReasons.constructor !== Array) {
+      console.warn('Reasons expects an array, you passed', typeof (definedReasons), definedReasons);
     }
 
-    // TODO - create a sub-component to pass handler and use props as the arguments
-    // @see https://stackoverflow.com/questions/29810914/react-js-onclick-cant-pass-value-to-method
     return (
       <div className={className} data-element="AnnotationReasonPopup">
         <div className="wrapper">
           <span className="reasonTitle">Redaction reason:</span>
           <ul className="reasonsList">
             {definedReasons.map(item =>
-              <li key={item} onClick={() => {
-                this.applyReason(item);
-              }}
+              <li key={item}
+                  onClick={() => {
+                    this.applyReason(item);
+                  }}
+                  className={(item === currentReason) ? 'current' : ''}
               >{item}</li>)}
             <li className="input-item">
               <div className="input-wrapper">
                 <input type="text"
                        placeholder="Enter custom reason"
                        autoComplete="off"
+                       value={this.state.inputValue}
+                       onChange={evt => this.updateInputValue(evt)}
                 />
               </div>
               <div className="button-wrapper">
                 <div className="button-save" onClick={() => {
-                  this.applyReason('TODO - UPDATE');
+                  this.applyReason(this.state.inputValue);
                 }}
-                >Save</div>
+                >Save
+                </div>
               </div>
             </li>
           </ul>
         </div>
       </div>
     );
+  }
+
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
   }
 }
 
