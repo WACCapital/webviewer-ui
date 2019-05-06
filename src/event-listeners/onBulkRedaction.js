4d37da5e5fd7855e65efa44359038fc0eb978c97
Curtis Morte <curtismorte@gmail.com>
2019-05-06 11:30:10 -0700

    Fix wrong keyname

--- src/event-listeners/onBulkRedaction.js
+++ src/event-listeners/onBulkRedaction.js
@@ -3,5 +3,5 @@ import fireEvent from '../helpers/fireEvent';
 
 export default dispatch => (e, values) => {
   dispatch(actions.closeElement('redactionModal'));
-  fireEvent('searchRedactionCompleted', {values: values.reason, result: values.results});
+  fireEvent('searchRedactionCompleted', {reason: values.reason, results: values.results});
 };
